d5c69db9dbc8c76292f19078b8ce520b9a94584a
thiagocalvi <thiagohcalvi@gmail.com>
2023-01-05 14:14:04 -0300

    change home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import { Footer } from './Components/Footer';
 import { Home } from './Views/Home';
 function App() {
   return (
     <div className="App">
-        <h1>onebit.fun</h1>
         <Home/>
-        {/* <Footer/> */}
     </div>
   );
 }
--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -2,9 +2,7 @@ import "./style.css"
 export function Home(){
     return(
         <div>
-           <button type="button"><a type="email" href="https://t.me/thelestnumber">Telegram</a></button>
-           <h2>Or</h2>
-           <button><a href="mailto:thiagohcalvi@gmail.com">Email</a></button>
+           <h1>Vou pensar em algo para colocar aqui em breve!</h1>
         </div>
     )
 }
